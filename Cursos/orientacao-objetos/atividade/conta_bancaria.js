2afdf6f11ab59d8bae566de07500afc468908bc5
vinicioslop <vinicioslop7@hotmail.com>
2022-02-04 18:16:05 -0300

    Finaliza o curso de Orientação a Objetos

---
+++ Cursos/orientacao-objetos/atividade/conta_bancaria.js
@@ -0,0 +1,75 @@
+class ContaBancaria {
+    constructor(agencia, numero, tipo) {
+        this.agencia = agencia;
+        this.numero = numero;
+        this.tipo = tipo;
+        this._saldo = 0;
+    }
+
+    get saldo() {
+        return this._saldo;
+    }
+
+    set saldo(valor) {
+        this._saldo = valor;
+    }
+
+    sacar(valor) {
+        if (valor > this._saldo)
+            return 'Operacao negada';
+
+        this._saldo = this._saldo - valor;
+
+        return this._saldo;
+    }
+
+    depositar(valor) {
+        this._saldo = this._saldo + valor;
+
+        return this._saldo;
+    }
+}
+
+class ContaCorrente extends ContaBancaria {
+    constructor(agencia, numero, cartaoCredito) {
+        super(agencia, numero);
+        this.tipo = 'corrente';
+        this._cartaoCredito = cartaoCredito;
+    }
+
+    get cartaoCredito() {
+        this._cartaoCredito;
+    }
+
+    set cartaoCredito(valor) {
+        this.cartaoCredito = valor;
+    }
+}
+
+class ContaPoupanca extends ContaBancaria {
+    constructor(agencia, numero) {
+        super(agencia, numero);
+        this.tipo = 'poupança';
+    }
+}
+
+class ContaUniversitaria extends ContaBancaria {
+    constructor(agencia, numero) {
+        super(agencia, numero);
+        this.tipo = 'universitária';
+    }
+
+    sacar(valor) {
+        if (valor > this.saldo)
+            return 'Operacao Negada!';
+
+        this._saldo = this._saldo - valor;
+    }
+}
+
+const minhaConta = new ContaCorrente(1, 234, true);
+minhaConta.saldo;
+minhaConta.depositar(1000);
+minhaConta.saldo;
+minhaConta.sacar(500);
+minhaConta.saldo;
